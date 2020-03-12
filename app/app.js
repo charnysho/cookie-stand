fa8744c94664b06f4158b1509562f39302ab415b
charnysho <charnysho@gmail.com>
2020-03-11 20:37:46 -0700

    add staff table and apply a control curve to the projected sales numbers

--- app/app.js
+++ app/app.js
@@ -15,6 +15,24 @@ function Store(location, maxCustomers, minCustomers, avgCookie) {
   };
 }
 
+Store.prototype.render = function() {
+  this.generateCookiesPerHour();
+  this.totalCalc();
+  var row = document.createElement('tr');
+  var locationCell = document.createElement('td');
+  locationCell.textContent = this.location;
+  row.appendChild(locationCell);
+  for(var cell = 0; cell < this.cookiesPerHour.length; cell++) {
+    var cookiesCell = document.createElement('td');
+    cookiesCell.textContent = Math.floor(this.cookiesPerHour[cell] * scale[cell]);
+    row.appendChild(cookiesCell);
+  }
+  var totalCell = document.createElement('td');
+  totalCell.textContent = this.total;
+  row.appendChild(totalCell);
+  return row;
+};
+
 var seattleLocation = new Store('Seattle', 65, 23, 6.3);
 var tokyoLocation = new Store('Tokyo', 24, 3, 1.2);
 var dubaiLocation = new Store('Dubai', 38, 11, 3.7);
@@ -38,10 +56,22 @@ var times = [
   '7pm',
 ];
 
+var scale = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
+
 var locations = [seattleLocation, tokyoLocation, dubaiLocation, parisLocation, limaLocation];
 
-function writeTable() {
+writeSalesTable();
+writeStaffTable();
+
+
+function writeSalesTable() {
   var tableEl = document.getElementById('table');
+  createCaptionRow(tableEl, 'Sales information');
+  tableEl.appendChild(createFirstRow(true));
+  render(locations, tableEl);
+}
+
+function createFirstRow(isCreateTotalCell) {
   var rowTime = document.createElement('tr');
   var emptyTimeCell = document.createElement('td');
   emptyTimeCell.textContent = '';
@@ -51,28 +81,53 @@ function writeTable() {
     timeCell.textContent = times[t];
     rowTime.appendChild(timeCell);
   }
-  var totalTimeCell = document.createElement('td');
-  totalTimeCell.textContent = 'Daily Location Total';
-  rowTime.appendChild(totalTimeCell);
-  tableEl.appendChild(rowTime);
+  if(isCreateTotalCell) {
+    var totalTimeCell = document.createElement('td');
+    totalTimeCell.textContent = 'Daily Location Total';
+    rowTime.appendChild(totalTimeCell);
+  }
+  return rowTime;
+}
+
+function render(locationsList, tableEl) {
+  for (var i = 0; i < locationsList.length; i++) {
+    var currentLocation = locationsList[i];
+    console.log(currentLocation);
+    tableEl.appendChild(currentLocation.render());
+  }
+}
+
+function createCaptionRow(tableEl, captionMessage) {
+  var caption = document.createElement('caption');
+  caption.textContent = captionMessage;
+  tableEl.appendChild(caption);
+}
+
+function writeStaffTable() {
+  var tableEl = document.getElementById('tableStaff');
+  createCaptionRow(tableEl, 'Staff information');
+  tableEl.appendChild(createFirstRow(false));
   for (var i = 0; i < locations.length; i++) {
     var currentLocation = locations[i];
-    currentLocation.generateCookiesPerHour();
-    currentLocation.totalCalc();
     var row = document.createElement('tr');
     var locationCell = document.createElement('td');
     locationCell.textContent = currentLocation.location;
     row.appendChild(locationCell);
     for(var cell = 0; cell < currentLocation.cookiesPerHour.length; cell++) {
+      var staff = calcValuesForSecondTable(currentLocation.cookiesPerHour[cell]);
       var cookiesCell = document.createElement('td');
-      cookiesCell.textContent = currentLocation.cookiesPerHour[cell];
+      cookiesCell.textContent = staff;
       row.appendChild(cookiesCell);
     }
-    var totalCell = document.createElement('td');
-    totalCell.textContent = currentLocation.total;
-    row.appendChild(totalCell);
     tableEl.appendChild(row);
   }
+}
+
+function calcValuesForSecondTable(cookiesSalesPerHour) {
+  var serveCustomersPerHour = 20;
+  var minimumNumberOfTossers = Math.ceil(cookiesSalesPerHour / serveCustomersPerHour);
+
+  return Math.max(minimumNumberOfTossers, 2);
 }
 
 function generateCookies(min, max, avg) {
@@ -84,8 +139,6 @@ function generateCookies(min, max, avg) {
   }
   return saledCookesPerHour;
 }
-
-writeTable();
 
 function calculateTotal(cookiesList) {
   var sum = 0;
